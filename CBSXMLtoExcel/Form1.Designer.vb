f5e9839fb93e66c57fe25a00a4376836d29e7b1c
Alex Svtwola <alex.cartagena@hotmail.com>
2015-05-02 17:17:21 +0200

    Initial Commit

---
+++ CBSXMLtoExcel/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.GridLines = True
+        Me.ListView1.Location = New System.Drawing.Point(33, 12)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(613, 217)
+        Me.ListView1.TabIndex = 0
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Name"
+        Me.ColumnHeader1.Width = 300
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Version"
+        Me.ColumnHeader2.Width = 120
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "ProcessorArchitecture"
+        Me.ColumnHeader3.Width = 120
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Language"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(571, 231)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 236)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(204, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "we will save the document with the name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(243, 236)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "%name%"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(36, 252)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "LOAD!"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(494, 257)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(152, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "(c) Visual Software Corporation"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(690, 279)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListView1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "CBS XML to XLSX"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ CBSXMLtoExcel/Form1.vb
@@ -0,0 +1,95 @@
+﻿Imports System.Xml
+Imports Microsoft.Office.Interop
+
+Public Class Form1
+
+    Dim filetoload As String = "FILENAMEHERE.xml"
+    Dim pkgname As String = ""
+    Dim pkgtype As String = ""
+    Dim ShellEXE As String
+    Dim CommandLineArgs As System.Collections.ObjectModel.ReadOnlyCollection(Of String) = My.Application.CommandLineArgs
+    Dim WorkShetNumber As Integer = 1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Application.CommandLineArgs.Count = 0 Then
+            'No args
+            MessageBox.Show("Error: No file to load. Please drag 'n drop a file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End
+        Else
+            'Args
+            For i As Integer = 0 To CommandLineArgs.Count - 1
+                ShellEXE = ShellEXE & " " & CommandLineArgs(i)
+            Next
+            filetoload = CommandLineArgs(0)
+            Button2_Click(sender, e)
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Dim doc As System.Xml.XmlDocument = New System.Xml.XmlDocument()
+            doc.Load(filetoload)
+            Dim nodes As XmlNodeList = doc.SelectNodes("/*[local-name()='Servicing']/*[local-name()='cbsItem']/*[local-name()='component']")
+            For Each node As XmlNode In nodes
+                Dim nodename As String = node.Attributes.GetNamedItem("name").InnerText
+                Dim nodever As String = node.Attributes.GetNamedItem("version").InnerText
+                Dim nodearch As String = node.Attributes.GetNamedItem("processorArchitecture").InnerText
+                Dim nodelang As String = node.Attributes.GetNamedItem("language").InnerText
+                Dim xmltoxls As New ListViewItem
+                xmltoxls.Text = nodename
+                xmltoxls.SubItems.Add(nodever)
+                xmltoxls.SubItems.Add(nodearch)
+                xmltoxls.SubItems.Add(nodelang)
+                ListView1.Items.Add(xmltoxls)
+            Next
+            Dim nodes2 As XmlNodeList = doc.SelectNodes("/*[local-name()='Servicing']/*[local-name()='cbsItem']")
+            For Each node As XmlNode In nodes2
+                pkgname = node.Attributes.GetNamedItem("name").InnerText
+                pkgtype = node.Attributes.GetNamedItem("type").InnerText
+            Next
+            Label2.Text = pkgname & "-" & pkgtype & ".xlsx"
+            Button1_Click(sender, e)
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim xls As New Excel.Application
+            Dim sheet As Excel.Worksheet
+            Dim i As Integer
+            xls.Workbooks.Add()
+            sheet = xls.ActiveWorkbook.Worksheets(WorkShetNumber)
+            Dim col As Integer = 1
+            For j As Integer = 0 To DirectCast(ListView1, ListView).Columns.Count - 1
+                sheet.Cells(1, col) = DirectCast(ListView1, ListView).Columns(j).Text.ToString
+                col = col + 1
+            Next
+            For i = 0 To DirectCast(ListView1, ListView).Items.Count - 1
+                Dim subitemscount As String = ""
+                Dim columnscount As String = DirectCast(ListView1, ListView).Columns.Count
+                Dim currentccount As Object = 1
+                Dim currentsubcount As Integer = 0
+                While currentccount <= columnscount
+                    sheet.Cells(i + 2, currentccount) = DirectCast(ListView1, ListView).Items.Item(i).SubItems(currentsubcount).Text
+                    currentccount = Val(currentccount) + 1
+                    currentsubcount = Val(currentsubcount) + 1
+                End While
+            Next
+            If IO.File.Exists(My.Application.Info.DirectoryPath & "\" & Label2.Text) = True Then
+                MessageBox.Show("Error, the output file already exists", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End
+            Else
+                xls.ActiveWorkbook.SaveAs(My.Application.Info.DirectoryPath & "\" & Label2.Text)
+            End If
+            xls.Workbooks.Close()
+            xls.Quit()
+            MessageBox.Show("Finished", "CBS XML to XLSX", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End
+        End Try
+    End Sub
+End Class
